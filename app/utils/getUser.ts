b1bb1ccc8e0f97ba713ad4e50840c772da1fc41f
shocker <fmju96@gmail.com>
2023-06-12 16:12:19 -0400

    Next project init

---
+++ app/utils/getUser.ts
@@ -0,0 +1,11 @@
+// https://nextjs.org/docs/app/building-your-application/data-fetching/caching#combining-cache-preload-and-server-only
+import { cache } from 'react'
+import 'server-only'
+ 
+export const preload = (category: string) => {
+  void getCategory(category)
+}
+ 
+export const getCategory = cache(async (category: string) => {
+  // ...
+})
